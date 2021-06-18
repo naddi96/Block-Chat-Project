2d3a70dd517a4e3a72bf6af8476bb623bad91fee
naddi96 <d.naddi.96@gmail.com>
2021-06-18 15:03:50 +0200

    create funzioni per compravenditda ma commentate

--- smartcontract/BlockChatback.sol
+++ smartcontract/BlockChatback.sol
@@ -133,14 +133,14 @@ contract NFT_MODEL is ERC721{
             prezzo[idtoken]=nuovo_prezzo;
             }
             */
-            /*
+
             function setForSale(uint256 _tokenId,uint256 prezzoVendita) public{
             address owner = ownerOf(_tokenId);
             //il prezzo del token non deve essere 0(mai stato settato) e il sender deve
             //essere il proprietario del token
             require(prezzo[_tokenId]==0);
             require(owner == msg.sender);
-           prezzo[_tokenId]=prezzoVendita;
+            prezzo[_tokenId]=prezzoVendita;
             //esegue l'approval della chiave del contratto, permettendo a quest ultimo
             //di accedere ai token ed effettuare modifiche
             }
@@ -188,9 +188,9 @@ contract NFT_MODEL is ERC721{
             function getReclamoFatto(uint256 id)public view returns ( bool){
                   return reclamo_fatto[id];
             }
-*/
 
-              function getNftInfo()public view returns ( nft_info memory){
+
+            function getNftInfo()public view returns ( nft_info memory){
                  nft_info memory nft= nft_info(nome_modello,
                            last_id,
                            pub_key_creatore,
@@ -202,6 +202,7 @@ contract NFT_MODEL is ERC721{
                            timestamp_creation);
                   return nft;
             }
+
 /*
 
                   function getTimestampDeadline()public view returns ( uint256){
@@ -255,7 +256,6 @@ contract NFT_MODEL is ERC721{
 */
       }
 
-
 contract BlockChat {
 
 
@@ -271,11 +271,6 @@ contract BlockChat {
       mapping (address => address) nft_address_to_creator;
 
       mapping (bytes => address) combkey_to_creator; //abi.encodePacked(msg.sender,nome_modello)
-
-
-
-
-
 
       function addNftComprati(address buyer,address nft_contr,uint256 id,string memory nome ) public  {
       require( nft_address_to_creator[msg.sender] != address(0),"operazione negata");
@@ -296,114 +291,60 @@ contract BlockChat {
                                           }
                                     address_to_id_nft[seller].pop();
                               }
-                        }
-      }
-
-
-
-
-
-      function getNftComprati(address addr)  public view returns (id_nft [] memory) {
-            return address_to_id_nft[addr];
-      }
-
-
-
-
-      event  ModelloNftCreato(address indexed _from,
-                              NFT_MODEL indirizzo_contratto,
-                              string  nome_modello,
-                              uint256 minuti_blocco,
-                              uint limite_messaggi,
-                              uint256 costo);
-
-
-/*
-
-uint256 constant USD_to_Wei = 430560661472170;
-msg.value;
-uint256 costoVero = calcolaCosto(limite_messaggi, minuti_blocco, tempo_validita);
-require(msg.value == 10000000000,"Il costo della creazione non coincide con il mio pene.");
-
-function log2(uint x) public returns (uint y){
-   assembly {
-        let arg := x
-        x := sub(x,1)
-        x := or(x, div(x, 0x02))
-        x := or(x, div(x, 0x04))
-        x := or(x, div(x, 0x10))
-        x := or(x, div(x, 0x100))
-        x := or(x, div(x, 0x10000))
-        x := or(x, div(x, 0x100000000))
-        x := or(x, div(x, 0x10000000000000000))
-        x := or(x, div(x, 0x100000000000000000000000000000000))
-        x := add(x, 1)
-        let m := mload(0x40)
-        mstore(m,           0xf8f9cbfae6cc78fbefe7cdc3a1793dfcf4f0e8bbd8cec470b6a28a7a5a3e1efd)
-        mstore(add(m,0x20), 0xf5ecf1b3e9debc68e1d9cfabc5997135bfb7a7a3938b7b606b5b4b3f2f1f0ffe)
-        mstore(add(m,0x40), 0xf6e4ed9ff2d6b458eadcdf97bd91692de2d4da8fd2d0ac50c6ae9a8272523616)
-        mstore(add(m,0x60), 0xc8c0b887b0a8a4489c948c7f847c6125746c645c544c444038302820181008ff)
-        mstore(add(m,0x80), 0xf7cae577eec2a03cf3bad76fb589591debb2dd67e0aa9834bea6925f6a4a2e0e)
-        mstore(add(m,0xa0), 0xe39ed557db96902cd38ed14fad815115c786af479b7e83247363534337271707)
-        mstore(add(m,0xc0), 0xc976c13bb96e881cb166a933a55e490d9d56952b8d4e801485467d2362422606)
-        mstore(add(m,0xe0), 0x753a6d1b65325d0c552a4d1345224105391a310b29122104190a110309020100)
-        mstore(0x40, add(m, 0x100))
-        let magic := 0x818283848586878898a8b8c8d8e8f929395969799a9b9d9e9faaeb6bedeeff
-        let shift := 0x100000000000000000000000000000000000000000000000000000000000000
-        let a := div(mul(x, magic), shift)
-        y := div(mload(add(m,sub(255,a))), shift)
-        y := add(y, mul(256, gt(arg, 0x8000000000000000000000000000000000000000000000000000000000000000)))
-    }  
+			}
 }
 
 
-//$NETTO = log(MEX)*(C_M)+ C_A / TIME^2 + C_S*EXP$
-function calcolaCosto(uint MEX, uint256 TIME, uint256 EXP) public returns (uint256){
-    uint256 netto = log2(MEX)*(10*USD_to_Wei)+ (20*USD_to_Wei) / (TIME*TIME)+(USD_to_Wei*20*EXP/100);
-    uint256 costo = 2*100/(netto)+netto;
-    return costo;
+
+
+
+function getNftComprati(address addr)  public view returns (id_nft [] memory) {
+      return address_to_id_nft[addr];
 }
 
-*/
 
-      function creaModelloNft(string memory nome_modello,
-                              uint256 minuti_blocco,
-                              uint limite_messaggi,
-                              uint limite_mint,
-                              uint256 tempo_validita,
-                              uint256 costo) public payable{
+event  ModelloNftCreato(address indexed _from,
+                        NFT_MODEL indirizzo_contratto,
+                        string  nome_modello,
+                        uint256 minuti_blocco,
+                        uint limite_messaggi,
+                        uint256 costo);
 
+function creaModelloNft(string memory nome_modello,
+                        uint256 minuti_blocco,
+                        uint limite_messaggi,
+                        uint limite_mint,
+                        uint256 tempo_validita,
+                        uint256 costo) public{
 
-
-
-
-      NFT_MODEL nft = new NFT_MODEL(
-                                          nome_modello,
-                                          msg.sender,
-                                          minuti_blocco,
-                                          limite_messaggi,
-                                          limite_mint,
-                                          tempo_validita,
-                                          costo,
-                                          address(this)
-                                          );
-
-
-      //combinazione due chiavi
-      bytes memory compKey = abi.encodePacked(msg.sender, bytes(nome_modello));
-      //require(combkey_to_creator[compKey]==address(0),"model already exist");
-      combkey_to_creator[compKey]=msg.sender;
-      nft_address_to_creator[address(nft)]= msg.sender;
-      emit ModelloNftCreato(msg.sender,
-                              nft,
-                              nome_modello,
+    
+    NFT_MODEL nft = new NFT_MODEL(
+                                    nome_modello,
+                                    msg.sender,
                                     minuti_blocco,
                                     limite_messaggi,
-                                    costo
+                                    limite_mint,
+                                    tempo_validita,
+                                    costo,
+                                    address(this)
                                     );
 
 
-      }
+    //combinazione due chiavi
+    bytes memory compKey = abi.encodePacked(msg.sender, bytes(nome_modello));
+    require(combkey_to_creator[compKey]==address(0),"model already exist");
+    combkey_to_creator[compKey]=msg.sender;
+    nft_address_to_creator[address(nft)]= msg.sender;
+    emit ModelloNftCreato(msg.sender,
+                          nft,
+                          nome_modello,
+                              minuti_blocco,
+                              limite_messaggi,
+                              costo
+                              );
+
+
+}
 
 
 
--- smartcontract/truffle-config.js
+++ smartcontract/truffle-config.js
@@ -9,7 +9,9 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "*"
+      network_id: "*",
+          
+      //gas:117000000  
     },
   //  test: {
   //    host: "127.0.0.1",
@@ -20,7 +22,11 @@ module.exports = {
 
   compilers: {
     solc: {
-      version: "0.8.0"
+      version: "0.8.0",
+      optimizer: {
+        enabled: true,
+        runs: 200
+    }
     }
   }
   //
